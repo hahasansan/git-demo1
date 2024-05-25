2ac96ccd44234ab253cf5e117ba087963328cf37
nenu <nenu@liu.com>
2024-05-25 23:11:02 +0800

    master fourth commit

--- src/main/java/com/example/GitDemo.java
+++ src/main/java/com/example/GitDemo.java
@@ -3,5 +3,6 @@ package com.example;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git5 by hot-fix");
+        System.out.println("hello git6 by master");
     }
 }
