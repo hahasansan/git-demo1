f025bf4bf59110d660531ebaa70c61f5ef73eb13
hahasansan <136994462+hahasansan@users.noreply.github.com>
2024-05-26 00:37:07 +0800

    Pull commit

--- src/main/java/com/example/GitDemo.java
+++ src/main/java/com/example/GitDemo.java
@@ -3,7 +3,7 @@ package com.example;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git5 by hot-fix");
-
+        System.out.println("pull git5 by hot-fix");
 
     }
 }
