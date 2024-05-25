ba04df28374d78ddd10b4b4963225244cd457067
nenu <nenu@liu.com>
2024-05-25 22:56:36 +0800

    hot-fix first commit

--- src/main/java/com/example/GitDemo.java
+++ src/main/java/com/example/GitDemo.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello git3");
+        System.out.println("hello git5 by hot-fix");
     }
 }
